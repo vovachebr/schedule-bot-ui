fb24d79d78d8e4e39e44fd85ab2c6d2490229de0
vovachebr <vovachebr@mail.ru>
2020-05-02 22:29:16 +0300

    Show templates

--- src/pages/templates-page/templates-page.js
+++ src/pages/templates-page/templates-page.js
@@ -2,27 +2,59 @@ import React from 'react';
 import { withSnackbar } from 'notistack';
 
 import { makeStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import Button from '@material-ui/core/Button';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 import { NoteAdd } from '@material-ui/icons';
 
 import CreateTemplateModal from './modal';
 
 const useStyles = makeStyles((theme) => ({
-    createTemplateButton: {
-        float: "left",
-        marginLeft: "5%",
+    template: {
         marginTop: "2%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center"
     },
+    buttonAdd: {
+      marginBottom: "5%"
+    },
+    list: {
+      width: "90%"
+    }
   }));
 
 function TemplatesPage({enqueueSnackbar}) {
   const classes = useStyles();
   const [openModal, setOpenModal] = React.useState(false);
+  const [templates, setTemplates] = React.useState([]);
+  const [isLoadingTemplate, setIsLoadingTemplate] = React.useState(false);
+
+  const getTemplates = () => {
+    setIsLoadingTemplate(true);
+    fetch('/templates', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    }).then(res => res.json()).then(res => {
+      setTemplates(res.templates);
+      setIsLoadingTemplate(false);
+    });
+  };
+
+  React.useEffect(() => {
+    if(!templates.length)
+      getTemplates();
+  });
   
   return (
-    <div>
+    <div className={classes.template}>
         <Button
-          className={classes.createTemplateButton}
+          className={classes.buttonAdd}
           variant="contained"
           color="primary"
           endIcon={<NoteAdd/>}
@@ -30,11 +62,25 @@ function TemplatesPage({enqueueSnackbar}) {
         >
         Создать шаблон сообщения
         </Button>
+        {isLoadingTemplate && <CircularProgress />}
         <CreateTemplateModal 
             open={openModal} 
             onCreate={(message)=> console.log(message)}
             onClose={() => setOpenModal(false)}
         />
+        {!isLoadingTemplate && <List
+          subheader={
+            <ListSubheader>
+              Список шаблонов
+            </ListSubheader>
+          }
+          className={classes.list}
+        >
+          {templates.map(t => 
+          <ListItem button>
+            <ListItemText primary={t.title} />
+          </ListItem>)}
+        </List>}
     </div>
   );
 }
